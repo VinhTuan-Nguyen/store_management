353b37a94eacccd612b3efd45f360eb1afa0cee1
Nguyen Vinh Tuan <vinhtuanbm@gmail.com>
2022-10-30 18:47:03 +0700

    create database

---
+++ database/db_store_management_tb_stogares.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE  IF NOT EXISTS `db_store_management` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_store_management`;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_store_management
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_stogares`
+--
+
+DROP TABLE IF EXISTS `tb_stogares`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_stogares` (
+  `c_item_code` varchar(12) NOT NULL,
+  `c_supplier` varchar(12) NOT NULL,
+  `c_mfg_date` date NOT NULL,
+  `c_exp_date` date NOT NULL,
+  `c_input_date` date NOT NULL,
+  `c_input_amount` int NOT NULL,
+  `c_batch_number` varchar(20) NOT NULL,
+  KEY `c_supplier_code_idx` (`c_supplier`),
+  KEY `c_item_code_idx` (`c_item_code`),
+  CONSTRAINT `c_item_code` FOREIGN KEY (`c_item_code`) REFERENCES `tb_items` (`c_item_code`),
+  CONSTRAINT `c_supplier_code` FOREIGN KEY (`c_supplier`) REFERENCES `tb_suppliers` (`c_supplier_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_stogares`
+--
+
+LOCK TABLES `tb_stogares` WRITE;
+/*!40000 ALTER TABLE `tb_stogares` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_stogares` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-30 18:44:43
